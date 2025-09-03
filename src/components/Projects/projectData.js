1f2e5329e88d2d12acaa45bd62d94cc955c5939c
Emmanuel326 <nyariboemmanuel8.com>
2025-09-03 13:58:56 +0300

    Add new project entry

--- src/components/Projects/projectData.js
+++ src/components/Projects/projectData.js
@@ -14,6 +14,14 @@ const projectData = [
     ghLink: "https://github.com/Emmanuel326/STKPUSH-Django",
     demoLink: "", // Add demo link if you have a live version
     tags: ["Django", " Python", " Backend", " API", " Payment Integration", " M-Pesa"]
+  },
+  {
+    title: "REACT-bootstrap portfolio website",
+    description: "A modern, responsive portfolio website built with React.js and react-bootstrap, designed as a central hub for my development work. This single-page application showcases my projects, skills, and experience with a clean, professional design",
+    imgPath: "", // You'll need to add this image
+    ghLink: "https://github.com/Emmanuel326/STKPUSH-Django",
+    demoLink: "", // Add demo link if you have a live version
+    tags: ["Django", " Python", " Backend", " API", " Payment Integration", " M-Pesa"]
   }
 ];
 
